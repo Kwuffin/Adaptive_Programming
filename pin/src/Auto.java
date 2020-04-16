f1a4fde945b7d8fe784e48f192f5bd3ece5e5de6
Kevin Wang <bestboykevin.bbg@gmail.com>
2020-04-16 15:48:43 +0200

    None.

---
+++ pin/src/Auto.java
@@ -0,0 +1,10 @@
+public class Auto {
+
+    private String kleur;
+    private String merk;
+    private String model;
+    private int aantalWielen;
+    private int bouwJaar;
+    private boolean heeftDak;
+
+}
