080618213953b67de18b38606ece64065307eae5
Kevin Wang <bestboykevin.bbg@gmail.com>
2020-04-16 20:45:49 +0200

    None.

--- AP/src/Klant.java
+++ AP/src/Klant.java
@@ -3,5 +3,27 @@ public class Klant {
     private String naam;
     private double kortingsPercentage;
 
+    public Klant(String nm, double korting){
+        naam = nm;
+        kortingsPercentage = korting;
+
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getKortingsPercentage() {
+        return kortingsPercentage;
+    }
+
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setKortingsPercentage(double kortingsPercentage) {
+        this.kortingsPercentage = kortingsPercentage;
+    }
 
 }
--- AP/src/Main.java
+++ AP/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
+        Klant k1 = new Klant("Pieter", 50.33);
+        System.out.println(k1.getNaam());
     }
 }
