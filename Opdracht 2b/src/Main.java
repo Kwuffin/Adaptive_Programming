774280b0eaa130ad16a6bbf3b889934584a744fe
Kevin Wang <bestboykevin.bbg@gmail.com>
2020-05-12 13:26:39 +0200

    Monte-Carlo FSM:
    50% chance based.
    13 total nodes.
    6 end-nodes.

--- Opdracht 2b/src/Main.java
+++ Opdracht 2b/src/Main.java
@@ -1,2 +1,64 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Node s0 = new Node(0);
+
+        Node s1 = new Node(1);
+        Node s2 = new Node(2);
+
+        Node s3 = new Node(3);
+        Node s4 = new Node(4);
+        Node s5 = new Node(5);
+        Node s6 = new Node(6);
+
+        Node endnote1 = new Node(7);
+        Node endnote2 = new Node(8);
+        Node endnote3 = new Node(9);
+        Node endnote4 = new Node(10);
+        Node endnote5 = new Node(11);
+        Node endnote6 = new Node(12);
+
+        s0.setNodeA(s1);
+        s0.setNodeB(s2);
+
+        s1.setNodeA(s3);
+        s1.setNodeB(s4);
+
+        s2.setNodeA(s5);
+        s2.setNodeB(s6);
+
+        s3.setNodeA(s1);
+        s3.setNodeB(endnote1);
+
+        s4.setNodeA(endnote2);
+        s4.setNodeB(endnote3);
+
+        s5.setNodeA(endnote4);
+        s5.setNodeB(endnote5);
+
+        s6.setNodeA(s2);
+        s6.setNodeB(endnote6);
+
+        ArrayList<Node> nodes= new ArrayList<>();
+        nodes.add(s0);
+        nodes.add(s1);
+        nodes.add(s2);
+        nodes.add(s3);
+        nodes.add(s4);
+        nodes.add(s5);
+        nodes.add(s6);
+        nodes.add(endnote1);
+        nodes.add(endnote2);
+        nodes.add(endnote3);
+        nodes.add(endnote4);
+        nodes.add(endnote5);
+        nodes.add(endnote6);
+
+        Fsm fsm1 = new Fsm(nodes);
+
+        fsm1.run();
+    }
 }
--- Opdracht 2b/src/Node.java
+++ Opdracht 2b/src/Node.java
@@ -1,2 +1,44 @@
+import java.util.Objects;
+
 public class Node {
+    public int id;
+    public Node nodeA;
+    public Node nodeB;
+
+    public Node(int id) {
+        this.id = id;
+    }
+
+    public Node nodeTransfer(int s){ // Parameter: either 0 or 1, random integer.
+        if (Objects.equals(s, 0)){
+            return getNodeA(); }
+
+        else if (Objects.equals(s, 1)){
+            return getNodeB(); }
+
+        else {
+            return null;
+        }
+    }
+
+    public void setNodeA(Node n) {
+        this.nodeA = n;
+    }
+
+    public void setNodeB(Node n) {
+        this.nodeB = n;
+    }
+
+    public Node getNodeA() {
+        return nodeA;
+    }
+
+    public Node getNodeB() {
+        return nodeB;
+    }
+
+    @Override
+    public String toString() {
+        return "" + id;
+    }
 }
