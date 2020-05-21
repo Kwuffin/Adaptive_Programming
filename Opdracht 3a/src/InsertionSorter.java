e4fdd6321c14c26f0b63f89f50554c3a1079f560
Kevin Wang <bestboykevin.bbg@gmail.com>
2020-05-21 20:47:47 +0200

    Insertion sort werkt.

--- Opdracht 3a/src/InsertionSorter.java
+++ Opdracht 3a/src/InsertionSorter.java
@@ -3,20 +3,22 @@ import java.util.ArrayList;
 public class InsertionSorter {
 
 
-
-    public InsertionSorter(){
+    public InsertionSorter() {
     }
 
-    public ArrayList<String> insertionSorter(ArrayList<String> iList){
+    public ArrayList<String> insertionSorter(ArrayList<String> iList) {
 
-        for(int i = 1; i < iList.size(); i++){
-            int j = i - 1;
+        for (int i = 1; i < iList.size(); i++) { //
+            String current = iList.get(i); // i is een iterator voor de lijst iList.
+            int j = i - 1; // We willen i vergelijken met de vorige String, dus index i-1, oftewel j.
 
-            while (j >= 0 && iList.get(j).compareToIgnoreCase(iList.get(i)) > 0) {
+            // While j is groter dan 0 (om IndexErrors te voorkomen)
+            // && compareToIgnoreCase is groter of gelijk aan 1, betekent dat j groter is dan i.
+            while ((j >= 0) && ((iList.get(j).compareToIgnoreCase(current)) >= 1)) { //CompareTo method gevonden dankzij het internet.
                 iList.set(j + 1, iList.get(j));
-                j = j - 1;
+                j--;
             }
-
+            iList.set(j + 1, current);
         }
         return iList;
     }
